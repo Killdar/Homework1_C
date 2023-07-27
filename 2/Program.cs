f47b9c5856e045392f6ede9ee4f1583ada3d0e42
Killdar <mykolahorobii@gmail.com>
2023-07-27 12:44:13 +0200

    homework 1

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = int.Parse(Console.ReadLine());
+int numberMax = 0;
+
+if (numberA > numberB)
+{
+    numberMax = numberA;
+}
+else
+{
+    numberMax = numberB;
+}
+
+if (numberMax < numberC)
+{
+    numberMax = numberC;
+}
+
+Console.WriteLine(numberMax);
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше {numberB}");
+}
+if (numberA < numberB)
+{
+    Console.WriteLine($"Число {numberA} меньше {numberB}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
